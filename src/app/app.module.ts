3ad0264a4a550cb2f72e12d041970bd47cfaae7d
Espejo <georgeespejo1@gmail.com>
2021-11-25 02:32:47 -0800

    Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,59 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { GalleryComponent } from './pages/gallery/gallery.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatDividerModule} from "@angular/material/divider";
+import {MatCardModule} from "@angular/material/card";
+import {MatListModule} from "@angular/material/list";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatSelectModule} from "@angular/material/select";
+import {MatOptionModule} from "@angular/material/core";
+import {FormsModule} from "@angular/forms";
+import {RouterModule, Routes} from "@angular/router";
+import { HomeComponent } from './pages/home/home.component';
+
+const routes: Routes  = [
+  {
+    path: "home", component:HomeComponent,
+    children: [
+      {path: "profile", component: ProfileComponent},
+      {path: "contacts", component: ContactComponent},
+      {path: "gallery", component: GalleryComponent},
+    ]
+  },
+  { path: "**", redirectTo: "home"}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    GalleryComponent,
+    ContactComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatDividerModule,
+    MatCardModule,
+    MatListModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatOptionModule,
+    FormsModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
